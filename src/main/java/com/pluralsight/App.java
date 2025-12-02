4ba6e6fd83e32cd4417ddb93681e6ebd597ea5b1
Vincentacheampong <Vincentacheampong10@gmail.com>
2025-12-02 09:58:50 -0500

    change to film, add columns

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -4,21 +4,30 @@ import java.sql.*;
 
 public class App {
     public static void main(String[] args) throws SQLException {
-        String url = "jdbc:mysql://localhost:3306/northwind";
+        String url = "jdbc:mysql://localhost:3306/Sakila";
         String username = args[0];
         String password = args[1];
 
         Connection connection = DriverManager.getConnection(url, username, password);
 
         Statement statement = connection.createStatement();
-        String query = "SELECT productName FROM products ";
+        String query = """
+                SELECT title, description, release_year, length
+                FROM film;
+                """;
 
 
         ResultSet results = statement.executeQuery(query);
 
         while (results.next()) {
-            String productName = results.getString("productName");
-            System.out.println(productName);
+            String title = results.getString("title");  ///   String title = results.getString("title"); and String description = results.getString(2); are the same
+            String description = results.getString(2);
+             int releaseYear = results.getInt(3);
+             int length = results.getInt(4);
+             System.out.println(title);
+            System.out.println(description);
+            System.out.println(releaseYear);
+            System.out.println(length);
         }
 
         connection.close();
