3390f6fdd1b8c1379d813bacad30237ecff2e34d
Vincentacheampong <Vincentacheampong10@gmail.com>
2025-12-02 09:40:48 -0500

    SELECT productName FROM products

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -5,8 +5,8 @@ import java.sql.*;
 public class App {
     public static void main(String[] args) throws SQLException {
         String url = "jdbc:mysql://localhost:3306/northwind";
-        String username = "root";
-        String password = "yearup";
+        String username = args[0];
+        String password = args[1];
 
         Connection connection = DriverManager.getConnection(url, username, password);
 
