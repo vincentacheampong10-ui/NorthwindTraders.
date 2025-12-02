a20a8f4d3d80178cbfa002a56049c2e4f22732a7
Vincentacheampong <Vincentacheampong10@gmail.com>
2025-12-02 11:44:51 -0500

    PreparedStatement for northwind exercise

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -4,35 +4,39 @@ import java.sql.*;
 
 public class App {
     public static void main(String[] args) throws SQLException {
-        String url = "jdbc:mysql://localhost:3306/Sakila";
+        String url = "jdbc:mysql://localhost:3306/Northwind";
         String username = args[0];
         String password = args[1];
 
         Connection connection = DriverManager.getConnection(url, username, password);
 
         String query = """
-                SELECT title, description, release_year, length
-                FROM film
-                WHERE title like ?
+                SELECT productID, productName, unitPrice, UnitsInStock
+                FROM Products
+                WHERE productName like ? or supplierID like ?;
                 """;
 
         PreparedStatement statement = connection.prepareStatement(query);
 
-        String searchTerm = "%AIR%";
-        statement.setString(1, searchTerm);
+        String productName = "%TOFU%";
+        int supplierID = 2;
+
+        statement.setString(1, productName);
+        statement.setInt(2, supplierID);
+
 
         ResultSet results = statement.executeQuery();
 
         while (results.next()) {
-            String title = results.getString("title");  ///   String title = results.getString("title"); and String description = results.getString(2); are the same
-            String description = results.getString(2);
-             int releaseYear = results.getInt(3);
-             int length = results.getInt(4);
+            int productID = results.getInt("productID");  ///   String title = results.getString("title"); and String description = results.getString(2); are the same
+            productName = results.getString(2);
+             int unitPrice = results.getInt(3);
+             int UnitsInStock = results.getInt(4);
 
-             System.out.println(title);
-            System.out.println(description);
-            System.out.println(releaseYear);
-            System.out.println(length);
+             System.out.println(productID);
+            System.out.println(productName);
+            System.out.println(unitPrice);
+            System.out.println(UnitsInStock);
         }
 
         results.close();
